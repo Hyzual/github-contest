a025d2db2e7e6bd7c1a96ea3051115f2bcacd408
Hyzual <hyzual@gmail.com>
2015-03-06 17:57:06 +0100

    Removes unnecessary Yeoman stuff, adds basic contestants choice
    Adds bare controllers and services

---
+++ app/scripts/controllers/main-controller.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('githubContestApp')
+.controller('MainController', ['$scope', function($scope){
+  $scope.contestants = [];
+
+  $scope.battle = function () {
+  };
+}]);
---
+++ app/scripts/services/github-service.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name githubContestApp.github
+ * @description
+ * # github
+ * Service in the githubContestApp.
+ */
+angular.module('githubContestApp')
+  .service('github', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
