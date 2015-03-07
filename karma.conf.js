41dff52332c523f7250c487076440b2dc50b1408
Hyzual <hyzual@gmail.com>
2015-03-07 12:16:22 +0100

    Add underscore and angular-underscore dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,8 @@ module.exports = function(config) {
       'bower_components/angular-route/angular-route.js',
       'bower_components/bootstrap/dist/js/bootstrap.js',
       'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+      'bower_components/underscore/underscore.js',
+      'bower_components/angular-underscore/angular-underscore.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/**/*.js',
